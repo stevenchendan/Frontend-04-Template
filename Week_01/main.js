c6f935682b62fa3ffc3af25e3a0128980bc3f2b3
stevenchendan <qwe16165850@gmail.com>
2020-08-30 22:51:33 +1000

    add click event

--- Week_01/main.js
+++ Week_01/main.js
@@ -2,23 +2,35 @@ let pattern = [
   [2, 0, 0],
   [0, 1, 0],
   [0, 0, 0]
-]
+];
+
+let color = 1;
 
 function show() {
   let boardWidth = 3;
   let boardHeight = 3;
   let board = document.getElementById("board");
+  //refresh the board
+  board.innerHTML = "";
   for (let i = 0; i < boardWidth; i++) {
     for (let j = 0; j < boardHeight; j++) {
       let cell = document.createElement("div");
       cell.classList.add("cell");
       cell.innerText = 
-        pattern[j][i] === 2 ? "❌" :
-        pattern[j][i] === 1 ? "⭕️" : "";
+        pattern[i][j] === 2 ? "❌" :
+        pattern[i][j] === 1 ? "⭕️" : "";
+      
+      cell.addEventListener("click", () => move(i, j));
       board.appendChild(cell);
     }
     board.appendChild(document.createElement("br"));
   }
+}
+
+function move(x, y) {
+  pattern[x][y] = color;
+  color = 3 - color;
+  show();
 }
 
 show(pattern);
