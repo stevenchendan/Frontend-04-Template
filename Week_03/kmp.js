a4b626453ce57c3be3a02eeb2afb9c528f1010c3
stevenchendan <qwe16165850@gmail.com>
2020-09-14 08:11:58 +1000

    finlize kmp algorithm

--- Week_03/kmp.js
+++ Week_03/kmp.js
@@ -8,25 +8,47 @@
 function kmp (source, pattern) {
   //compute table
   let table = new Array(pattern.length).fill(0);
-  
-  let i = 0, j = 0;
-  
-  while (i < pattern.length) {
-    if (pattern[i] === pattern[j]) {
-      ++j, ++i;
-      table[i] = j;
-    } else {
-      if (j > 0) {
-        j = table[j];
-      } else {
+  {
+    //make the variable to be local scope rather than global scope
+    let i = 0, j = 0;
+    while (i < pattern.length) {
+      if (pattern[i] === pattern[j]) {
+        ++j, ++i;
         table[i] = j;
-        ++i;
+      } else {
+        if (j > 0) {
+          j = table[j];
+        } else {
+          table[i] = j;
+          ++i;
+        }
       }
     }
   }
+  
+  {
+    //i is the index of source, j is the index of pattern
+    let i = 0, j = 0;
+    while (i < source.length) {
+      if (j === pattern.length) {
+        return true;
+      }
+      if (pattern[j] === source[i]) {
+        ++i, ++j;
+      } else {
+        if (j > 0) {
+          j = table[j];
+        } else {
+          table[i] = j;
+          ++i;
+        }
+      }
+    }
+    return false;
+  }
   console.log(table);
-  //
-
 }
 
 kmp("", "abcdabcd")
+
+
