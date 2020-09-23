b1b64403052e49f092cc3b4e4e7b67ee8b3291ed
stevenchendan <qwe16165850@gmail.com>
2020-09-23 21:18:47 +1000

    add object in week 4

---
+++ Week_04/object.js
@@ -0,0 +1,17 @@
+class Dog {
+  constructor(name) {
+      this.name = name;
+  }
+  bite(name) {
+    console.log(`I bite ${name}`);
+  }
+}
+
+class Human {
+  constructor(name) {
+      this.name = name;
+  }
+  hurt(name) {
+    console.log(`I hurt by ${name}`);
+  }
+}
