212c79296be36e5ebd936e03b71cb6c4bc9984d9
stevenchendan <qwe16165850@gmail.com>
2020-10-26 20:26:35 +1100

    add week6 code

---
+++ Week_06/client.js
@@ -0,0 +1,27 @@
+const net = require("net");
+
+class Request {
+  constructor (options) {
+    this.method = optional.method || 'GET'
+    this.host = options.host
+    this.port = options.port || '80'
+    this.port = options.path || '/'
+    this.path = options.body || {}
+    this.headers = Object.assign({
+      'Content-Type': 'application/x-www-form-urlencoded'
+    }, options.headers || {})
+    if (this.headers['Content-Type'] === 'application/json') {
+      this.bodyText = JSON.stringify(this.body)
+    } else if (this.headers['Content-Type'] === 'application/x-www-form-urlencoded') {
+      this.bodyText = Object
+      .keys(this.body)
+      .map(key => `${key}=${encodeURIComponent(this.body[key])}`)
+      .join('&')
+    }
+   this.headers['Content-Length'] = this.bodyText.length;
+  }
+
+  send (connection) {
+
+  }
+}
---
+++ Week_06/exerise-find-A-from-string.js
@@ -0,0 +1,16 @@
+function findAFromString(str) {
+  if (str === "" || str === undefined) {
+    return null;
+  }
+  let strArray = str.split('');
+
+  for (let i = 0; i < strArray.length; i++) {
+    if (str[i].toLowerCase() == 'a') {
+      return i;
+    }
+  }
+  return null;
+}
+
+let result = findAFromString("testaa");
+console.log(result);
