ac88d4041b481b7b1d3e13849d41c70cf19c9e1a
stevenchendan <qwe16165850@gmail.com>
2020-11-09 20:32:49 +1100

    implement tiny browser client and server

--- Week_06/client.js
+++ Week_06/client.js
@@ -22,6 +22,6 @@ class Request {
   }
 
   send (connection) {
-
+    
   }
 }
---
+++ Week_06/tiny-browser/server.js
@@ -0,0 +1,18 @@
+const http = require('http');
+
+http.createServer((request, response) => {
+  let body = []
+  // listen to three possible condition error, data and end
+  request.on('error', (err) => {
+    console.error(err)
+  }).on('data', (chunk) => {
+    body.push(chunk.toString());
+  }).on('end',() => {
+    body = Buffer.concat(body).toString();
+    console.log("body:", body);
+    response.writeHead(200, {'Content-type': 'text/html'});
+    response.end('Hello World\n');
+  });
+}).listen(8088);
+
+console.log("server started");
