c6d4cbdaa2958df1369408a987443804b2da0202
stevenchendan <qwe16165850@gmail.com>
2020-11-09 22:27:07 +1100

    update client code

--- Week_06/tiny-browser/client.js
+++ Week_06/tiny-browser/client.js
@@ -21,10 +21,31 @@ class Request {
     this.headers["Content-Length"] = this.bodyText.length;
   }
 
-  send () {
+  send (connection) {
     return new Promise((resolve, reject) => {
       const parser = new ResponseParser;
-      resolve("");
+      if (connection) {
+        connection.write(this.toString());
+      } else {
+        connection = net.createConnection({
+          host: this.host,
+          port: this.port
+        }, () => {
+          connection.write(this.toString());
+        })
+      }
+      connection.on('data', (data) => {
+        console.log(data.toString());
+        parser.receive(data.toString());
+        if (parser.isFinished) {
+          resolve(parser.response);
+          connection.end();
+        }
+      });
+      connection.on('error', (err) => {
+        reject(err);
+        connection.end();
+      });
     })
   }
 }
