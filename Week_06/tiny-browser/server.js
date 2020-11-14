f90375026ef9dfc89df91a258200b420e5bb722c
stevenchendan <qwe16165850@gmail.com>
2020-11-14 21:16:44 +1100

    update client and server code for browser

--- Week_06/tiny-browser/server.js
+++ Week_06/tiny-browser/server.js
@@ -2,17 +2,16 @@ const http = require('http')
 
 http.createServer((request, response) => {
   let body = []
-  // listen to three possible condition error, data and end
-  request.on('error', (err) => {
+  request.on('error', err => {
     console.error(err)
-  }).on('data', (chunk) => {
+  }).on('data', chunk => {
     body.push(chunk.toString())
   }).on('end', () => {
-    body = Buffer.concat(body).toString()
-    console.log('body:', body)
-    response.writeHead(200, { 'Content-type': 'text/html' })
-    response.end('Hello World\n')
+    // body = Buffer.concat(body).toString()
+    console.log('body: ' + body)
+    response.writeHead(200, { 'Content-Type': 'text/html' })
+    response.end(' hello world\n')
   })
 }).listen(8088)
 
-console.log('server started')
+console.log('server started on http://localhost:8088')
